37e38d80fa01127e8c36fc2c85991c030be2f4c6
Hettore Eduardo <duh.barretoo@gmail.com>
2022-08-10 20:18:00 -0300

    Resolvendo o exercício 7 sobre estruturas condicionais.

--- src/treinando/Program.java
+++ src/treinando/Program.java
@@ -207,6 +207,32 @@ public class Program {
 		
 		*/
 		
+		// Resolucao:
+		
+		double x, y;
+		
+		System.out.println("por favor insira a coordenada de x: ");
+		x = sc.nextDouble();
+		System.out.println("por favor insira a coordenada de y: ");
+		y = sc.nextDouble();
+		
+		if (x == 0 && y == 0) {
+			System.out.println("Origem.");
+		} else if (x > 0 && y > 0) {
+			System.out.println("Q1");
+		} else if (x < 0 && y > 0) {
+			System.out.println("Q2");
+		} else if (x < 0 && y < 0) {
+			System.out.println("Q3");
+		} else if (x > 0 && y < 0) {
+			System.out.println("Q4");
+		} else if (x == 0 && y != 0) {
+			System.out.println("Eixo y");
+		} else if (y== 0 && x != 0) {
+			System.out.println("Eixo x");
+		} else {
+			System.out.println("FIM.");
+		}
 		
 		
 	}
