3a060a8b580795e6f842c39a15e013d8ad86150b
Hettore Eduardo <duh.barretoo@gmail.com>
2022-08-11 10:25:53 -0300

    Resolvendo o 8º exercício da lista de estruturas concicionais.

--- src/treinando/Program.java
+++ src/treinando/Program.java
@@ -95,148 +95,160 @@ public class Program {
 		// Resolucao:
 
 		/*
-		int horaInicial, horaFinal;
+		 * int horaInicial, horaFinal;
+		 * 
+		 * horaInicial = sc.nextInt(); horaFinal = sc.nextInt();
+		 * 
+		 * int duracao;
+		 * 
+		 * if (horaInicial < horaFinal) { duracao = horaFinal - horaInicial; } else {
+		 * duracao = 24 - horaInicial + horaFinal; }
+		 * 
+		 * System.out.println("O JOGO DUROU " + duracao + " HORA(S)");
+		 * 
+		 * sc.close();
+		 */
 
-		horaInicial = sc.nextInt();
-		horaFinal = sc.nextInt();
-
-		int duracao;
-
-		if (horaInicial < horaFinal) {
-			duracao = horaFinal - horaInicial;
-		} else {
-			duracao = 24 - horaInicial + horaFinal;
-		}
-
-		System.out.println("O JOGO DUROU " + duracao + " HORA(S)");
-
-		sc.close();
-		*/
-		
 		/*
-		Exercicos 5 Com base na tabela abaixo, escreva um programa que leia o 
-		código de um item e a quantidade deste item. A
-		seguir, calcule e mostre o valor da conta a pagar.
-		
-		CODIGO: 1 ESPECIFICACAO: Cachorro Quente PRECO: R$ 4.00
-		CODIGO: 2 ESPECIFICACAO: X-Salada PRECO: R$ 4.50
-		CODIGO: 3 ESPECIFICACAO: X-Bacon PRECO: R$ 5.00
-		CODIGO: 4 ESPECIFICACAO: Torrada Simples PRECO: R$ 2.00
-		CODIGO: 5 ESPECIFICACAO: Refrigerante PRECO: R$ 1.50
-		
-		Entrada: 3 e 2 Saida: TOTAL: R$ 10.00
-		Entrada: 2 e 3 Saida: TOTAL: R$ 13.50
-		
-		*/
-		
-		//Resolucao:
-		
-		/*
-		int codigo;
-		int quantidade;
-		
-		System.out.println("Por favor digite o codigo do produto: ");
-		codigo = sc.nextInt();
-		System.out.println("Agora digite a quantidade: ");
-		quantidade = sc.nextInt();
-		
-		double preco = 0;
-		
-		if (codigo == 1) {
-			preco = quantidade * 4.00;
-		} else if (codigo == 2) {
-			preco = quantidade * 4.50;
-		} else if (codigo == 3) {
-			preco = quantidade * 5.00;
-		} else if (codigo == 4) {
-			preco = quantidade * 2.00;
-		} else if (codigo == 5) {
-			preco = quantidade * 1.50;
-		} else {
-			System.out.println("Por favor digite um valor valido no menu!");
-		}
-		
-		
-		System.out.printf("TOTAL: R$ %.2f%n", preco);
-		
-		sc.close();
-		*/
-		
-		/*
-		Exercicio 6 Você deve fazer um programa que leia um valor qualquer e 
-		apresente uma mensagem dizendo em qual dos seguintes intervalos 
-		([0,25], (25,50], (50,75], (75,100]) este valor se encontra. Obviamente 
-		se o valor não estiver em nenhum destes intervalos, deverá ser impressa 
-		a mensagem “Fora de intervalo”.
-		
-		Entrada: 25.01 Saida: Intervalo (25,50)
-		Entrada: 25.00 Saida: Intervalo (0,25)
-		Entrada: 100.00 Saida: Intervalo (75,100)
-		Entrada: -25.02 Saida: Fora do intervalo
-		*/
-		
+		 * Exercicos 5 Com base na tabela abaixo, escreva um programa que leia o código
+		 * de um item e a quantidade deste item. A seguir, calcule e mostre o valor da
+		 * conta a pagar.
+		 * 
+		 * CODIGO: 1 ESPECIFICACAO: Cachorro Quente PRECO: R$ 4.00 CODIGO: 2
+		 * ESPECIFICACAO: X-Salada PRECO: R$ 4.50 CODIGO: 3 ESPECIFICACAO: X-Bacon
+		 * PRECO: R$ 5.00 CODIGO: 4 ESPECIFICACAO: Torrada Simples PRECO: R$ 2.00
+		 * CODIGO: 5 ESPECIFICACAO: Refrigerante PRECO: R$ 1.50
+		 * 
+		 * Entrada: 3 e 2 Saida: TOTAL: R$ 10.00 Entrada: 2 e 3 Saida: TOTAL: R$ 13.50
+		 * 
+		 */
+
 		// Resolucao:
-		
+
 		/*
-		double valor;
-		
-		valor = sc.nextDouble();
-		
-		if ( valor >= 0 && valor <= 25 ) {
-			System.out.println("Intervalo de (0 a 25)");
-		} else if ( valor > 25 && valor <= 50) {
-			System.out.println("Intervalo de (25 a 50)");
-		} else if ( valor > 50 && valor <= 75) {
-			System.out.println("Intervalo de (50 a 75)");
-		} else if ( valor > 75 && valor <= 100) {
-			System.out.println("Intervalo de 75 a 100");
-		} else {
-			System.out.println("Fora do intervalo");
-		}
-		*/
-		
+		 * int codigo; int quantidade;
+		 * 
+		 * System.out.println("Por favor digite o codigo do produto: "); codigo =
+		 * sc.nextInt(); System.out.println("Agora digite a quantidade: "); quantidade =
+		 * sc.nextInt();
+		 * 
+		 * double preco = 0;
+		 * 
+		 * if (codigo == 1) { preco = quantidade * 4.00; } else if (codigo == 2) { preco
+		 * = quantidade * 4.50; } else if (codigo == 3) { preco = quantidade * 5.00; }
+		 * else if (codigo == 4) { preco = quantidade * 2.00; } else if (codigo == 5) {
+		 * preco = quantidade * 1.50; } else {
+		 * System.out.println("Por favor digite um valor valido no menu!"); }
+		 * 
+		 * 
+		 * System.out.printf("TOTAL: R$ %.2f%n", preco);
+		 * 
+		 * sc.close();
+		 */
+
 		/*
-		Exercicio 7 Leia 2 valores com uma casa decimal (x e y), 
-		que devem representar as coordenadas de um ponto em um plano. 
-		A seguir, determine qual o quadrante ao qual pertence o
-		ponto, ou se está sobre um dos eixos cartesianos ou na 
-		origem (x = y = 0). Se o ponto estiver na origem, escreva a 
-		mensagem “Origem”. Se o ponto estiver sobre um dos eixos escreva 
-		“Eixo X” ou “Eixo Y”, conforme for a situação.
-		
-		
-		*/
-		
+		 * Exercicio 6 Você deve fazer um programa que leia um valor qualquer e
+		 * apresente uma mensagem dizendo em qual dos seguintes intervalos ([0,25],
+		 * (25,50], (50,75], (75,100]) este valor se encontra. Obviamente se o valor não
+		 * estiver em nenhum destes intervalos, deverá ser impressa a mensagem “Fora de
+		 * intervalo”.
+		 * 
+		 * Entrada: 25.01 Saida: Intervalo (25,50) Entrada: 25.00 Saida: Intervalo
+		 * (0,25) Entrada: 100.00 Saida: Intervalo (75,100) Entrada: -25.02 Saida: Fora
+		 * do intervalo
+		 */
+
 		// Resolucao:
-		
+
 		/*
-		double x, y;
-		
-		System.out.println("por favor insira a coordenada de x: ");
-		x = sc.nextDouble();
-		System.out.println("por favor insira a coordenada de y: ");
-		y = sc.nextDouble();
-		
-		if (x == 0 && y == 0) {
-			System.out.println("Origem.");
-		} else if (x > 0 && y > 0) {
-			System.out.println("Q1");
-		} else if (x < 0 && y > 0) {
-			System.out.println("Q2");
-		} else if (x < 0 && y < 0) {
-			System.out.println("Q3");
-		} else if (x > 0 && y < 0) {
-			System.out.println("Q4");
-		} else if (x == 0 && y != 0) {
-			System.out.println("Eixo y");
-		} else if (y== 0 && x != 0) {
-			System.out.println("Eixo x");
-		} else {
-			System.out.println("FIM.");
+		 * double valor;
+		 * 
+		 * valor = sc.nextDouble();
+		 * 
+		 * if ( valor >= 0 && valor <= 25 ) {
+		 * System.out.println("Intervalo de (0 a 25)"); } else if ( valor > 25 && valor
+		 * <= 50) { System.out.println("Intervalo de (25 a 50)"); } else if ( valor > 50
+		 * && valor <= 75) { System.out.println("Intervalo de (50 a 75)"); } else if (
+		 * valor > 75 && valor <= 100) { System.out.println("Intervalo de 75 a 100"); }
+		 * else { System.out.println("Fora do intervalo"); }
+		 */
+
+		/*
+		 * Exercicio 7 Leia 2 valores com uma casa decimal (x e y), que devem
+		 * representar as coordenadas de um ponto em um plano. A seguir, determine qual
+		 * o quadrante ao qual pertence o ponto, ou se está sobre um dos eixos
+		 * cartesianos ou na origem (x = y = 0). Se o ponto estiver na origem, escreva a
+		 * mensagem “Origem”. Se o ponto estiver sobre um dos eixos escreva “Eixo X” ou
+		 * “Eixo Y”, conforme for a situação.
+		 * 
+		 * 
+		 */
+
+		// Resolucao:
+
+		/*
+		 * double x, y;
+		 * 
+		 * System.out.println("por favor insira a coordenada de x: "); x =
+		 * sc.nextDouble(); System.out.println("por favor insira a coordenada de y: ");
+		 * y = sc.nextDouble();
+		 * 
+		 * if (x == 0 && y == 0) { System.out.println("Origem."); } else if (x > 0 && y
+		 * > 0) { System.out.println("Q1"); } else if (x < 0 && y > 0) {
+		 * System.out.println("Q2"); } else if (x < 0 && y < 0) {
+		 * System.out.println("Q3"); } else if (x > 0 && y < 0) {
+		 * System.out.println("Q4"); } else if (x == 0 && y != 0) {
+		 * System.out.println("Eixo y"); } else if (y== 0 && x != 0) {
+		 * System.out.println("Eixo x"); } else { System.out.println("FIM."); }
+		 */
+
+		/*
+		 * Exercício 8 Em um país imaginário denominado Lisarb, todos os habitantes
+		 * ficam felizes em pagar seus impostos, pois sabem que nele não existem
+		 * políticos corruptos e os recursos arrecadados são utilizados em benefício da
+		 * população, sem qualquer desvio. A moeda deste país é o Rombus, cujo símbolo é
+		 * o R$. Leia um valor com duas casas decimais, equivalente ao salário de uma
+		 * pessoa de Lisarb. Em seguida, calcule e mostre o valor que esta pessoa deve
+		 * pagar de Imposto de Renda, segundo a tabela abaixo.
+		 * 
+		 * Renda: de 0.00 a R$ 2000.00 Imposto de renda: Isento; Renda: de R$ 2000.01
+		 * até R$ 3000.00 Imposto de renda: 8%; Renda: de R$ 3000.01 até R$ 4500.00
+		 * Imposto de renda: 18%; Renda: Acima de 4500.00 Imposto de renda: 28%;
+		 * 
+		 * Lembre que, se o salário for R$ 3002.00, a taxa que incide é de 8% apenas
+		 * sobre R$ 1000.00, pois a faixa de salário que fica de R$ 0.00 até R$ 2000.00
+		 * é isenta de Imposto de Renda. No exemplo fornecido (abaixo), a taxa é de 8%
+		 * sobre R$ 1000.00 + 18% sobre R$ 2.00, o que resulta em R$ 80.36 no total. O
+		 * valor deve ser impresso com duas casas decimais.
+		 */
+
+		// Resolução:
+
+		/*
+		double salario;
+		double imposto;
+		//double imposto2;
+
+		System.out.println("Por favor digite o valor do salário a ser calculado: ");
+		salario = sc.nextDouble();
+
+		if (salario < 2000.00) {
+			System.out.println("Imposto de reda: Isento.");
+		} else if (salario > 2000.00 && salario < 3000.00) {
+			imposto = (salario - 2000.00) * 8 / 100;
+			System.out.printf("Impostos: R$ %.2f%n ", imposto);
+		} else if (salario > 3000.00 && salario < 4500.00) {
+			imposto = (salario - 3000.00) * 18 / 100 + 1000.00 * 8 / 100;
+			System.out.printf("Impostos: R$ %.2f%n ", imposto);
+		} // else if (salario > 4500.00) {
+			// imposto = (salario - 4500.00) * 28/100 + 1500.00 * 18/100 + 1000.00 *8/100;
+			// System.out.printf("Imposto: %.2f%n ", imposto);
+		// }
+		else {
+			imposto = (salario - 4500.00) * 28 / 100 + 1500.00 * 18 / 100 + 1000.00 * 8 / 100;
+			System.out.printf("Imposto: R$ %.2f%n ", imposto);
 		}
 		*/
-		
-		
 	}
 
 }
